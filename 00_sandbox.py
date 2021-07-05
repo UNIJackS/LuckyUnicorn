74afaf997ad8229bd90e650827598304a9bf20f5
scrive.jack <scrve.jack@awatapu.school.nz>
2021-07-05 12:00:19 +1200

    added code that asks for a number then multipys it by 5 and prints the result

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# asking for a number and makeing sure it an int
+get_number = int(input("chose a number"))
+
+# multipyls the input by 5
+total = get_number * 5
+
+# prints the output
+print("{} multiplyed by 5 is equal to {}".format(get_number,total))
