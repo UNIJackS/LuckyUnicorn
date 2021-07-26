97e8d4fe7b4c1a2a5fa6a5104ced18a620202aa6
scrive.jack <scrve.jack@awatapu.school.nz>
2021-07-26 12:52:46 +1200

    basic yes no checker, asks if they have played before and then prints instructions if they have not. The program also will not accept any other answer then a Y,yes,N or No.

---
+++ 07_yes_no_v1.py
@@ -0,0 +1,24 @@
+# loops the code till yes or no is entered
+show_instructions = "***"
+while show_instructions != "xxx":
+
+# ask the user if they have played before
+    show_instructions =  input("Have you played before ?").lower()
+
+# If they say yes 'program continues'
+    if show_instructions == "yes":
+      print("program continues")
+
+    elif show_instructions == "y":
+      print("program continues")
+
+    elif show_instructions == "No":
+        print("Displys instructions")
+
+    elif show_instructions == "N":
+        print("Displys instructions")
+
+# If they say no 'display instructions'
+    else:
+        print("Please enter yes or No")
+
