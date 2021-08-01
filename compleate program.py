c1bd3793ecf18f0c631e894763542821723724ca
10Jack2 <86272782+10Jack2@users.noreply.github.com>
2021-08-01 21:33:37 +1200

    Update compleate program.py
    added the number checker function to the main program

--- compleate program.py
+++ compleate program.py
@@ -27,6 +27,30 @@ def instructions():
     print()
     return ""
 
+# function that checks numbers are withen a range
+def number_checker(question,low,high):
+    # sets the error statement for the if statement to follow
+    error = "please enter a whole number between 1 and 10\n"
+
+    # keeps the if statement looping until an appropriate answer is entered
+    valid = False
+    while not valid:
+
+        # This try statment makes it so the program does not crash if a float is entered rather then an interger as the response
+        try:
+            response = int(input(question))
+
+            # An if statement that checks if the response to the previous statement is between 0 and 10
+            if low < response <= high:
+                return(response)
+
+            # An if statement that outputs the error statement if the response is not between 0 and 10
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
 # main routine
 
 # Asks the user if they have played before then uses the yes_no function to determine what they said
@@ -34,5 +58,9 @@ show_instructions = yes_no("Have you played before ?")
 # Shows instructions if no was the answer to the above question
 if show_instructions == "no":
     instructions()
+
+how_much = number_checker("How much would you like to play with ? ", 0, 10)
+
+print("You have asked to play with ${}".format(how_much))
 
 print("program continues")
